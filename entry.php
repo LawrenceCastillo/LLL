c29ecbeac27c2a1c62946e1f2c09e57b55a0e5b9
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2019-12-04 22:14:38 -0500

    quiz expansion

--- entry.php
+++ entry.php
@@ -4,85 +4,59 @@ session_start();
 // Include config file
 require_once "config.php";
 
-$id = $_SESSION['id'];
+$id    = $_SESSION['id'];
+$count = 0;
 
+foreach ($_POST as $key => $value) {
+        echo "<tr>";
+        echo "<td>";
+        echo $key;
+        echo "</td>";
+        echo "<td>";
+        echo $value;
+        echo "</td>";
+        echo "</tr>";
+    }
 
 // Check which question was answered and which choice selected
-if ($_POST['q2'] == "Tend to Agree") {
-	$choice_id = 2*2;
-	echo 'affirmative';
-}
-else if ($_POST['q2'] == "Tend to Disagree") {
-  $choice_id = 2*2+1;
+if (!isset($_POST['q2'])){
+  if ( $_POST['q2'] == "Tend to Agree" ){ $choice_id = 2*2; $count++; }
+  else if ( $_POST['q2'] == "Tend to Disagree" ){ $choice_id = 2*2+1; $count++; }
 }
 
-echo $choice_id;
+if ($stmt = $con->prepare('INSERT INTO chooses (account_id, choice_id) VALUES (?, ?)')) {
+  $stmt->bind_param('ss', $id, $choice_id);
+  $stmt->execute();
+} else { die ( 'Something went wrong!' ); }
+$stmt->close();
 
-// Now we check if the data was submitted, isset() function will check if the data exists.
-//if (!isset($_POST['phonenumber'], $_POST['zipcode'], $_POST['lookingfor'])) {
-  // Could not get the data that should have been sent.
-//  die ('Please complete the registration form!');
-//}
+if (!isset($_POST['q3'])){
+  if ( $_POST['q3'] == "Tend to Agree" ){ $choice_id = 3*2; $count++; }
+  else if ( $_POST['q3'] == "Tend to Disagree" ){ $choice_id = 3*2+1; $count++; }
+}
+
+if ($stmt = $con->prepare('INSERT INTO chooses (account_id, choice_id) VALUES (?, ?)')) {
+  $stmt->bind_param('ss', $id, $choice_id);
+  $stmt->execute();
+} else { die ( 'Something went wrong!' ); }
+$stmt->close();
+
+if (!isset($_POST['q4'])){
+  if ( $_POST['q4'] == "Tend to Agree" ) { $choice_id  = 4*2; $count++; }
+  else if ( $_POST['q4'] == "Tend to Disagree" ){ $choice_id = 4*2+1; $count++; }
+}
+
+if ($stmt = $con->prepare('INSERT INTO chooses (account_id, choice_id) VALUES (?, ?)')) {
+  $stmt->bind_param('ss', $id, $choice_id);
+  $stmt->execute();
+} else { die ( 'Something went wrong!' ); }
+$stmt->close();
+
 // Make sure the submitted registration values are not empty.
 //if (empty($_POST['phonenumber']) || empty($_POST['zipcode']) || empty($_POST['lookingfor'])) {
   // One or more values are empty.
 //  die ('Please complete the registration form');
 //}
-
-// Check for valid email
-//if (preg_match('/[0-9\-]+/', $_POST['phonenumber']) == 0) {
-//  die ('Phone number is not valid!');
-//}
-
-// Check for valid zipcode
-//if (preg_match('/[0-9]+/', $_POST['zipcode']) == 0) {
-//  die ('Zipcode is not valid!');
-//}
-/*
-// Check valid char length
-if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
-  die ('Password must be between 5 and 20 characters long!');
-}
- */
-/*
-// We need to check if the account with that username exists.
-if ($stmt = $con->prepare('SELECT account_id, password FROM accounts WHERE username = ?')) {
-  // Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
-  $stmt->bind_param('s', $_POST['username']);
-  $stmt->execute();
-  $stmt->store_result();
-  // Store the result so we can check if the account exists in the database.
-  if ($stmt->num_rows > 0) {
-    // Username already exists
-    echo 'Username exists, please choose another!';
-  } else {
-    // Username doesnt exists, insert new account
-    if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
-    // We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
-      $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-      $stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
-      $stmt->execute();
-      echo 'You have successfully registered, you can now login!';
-    } else {
-      // Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
-      echo 'Could not prepare statement!';
-    }
-  }
-  $stmt->close();
-} else {
-  // Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
-  echo 'Could not prepare statement!';
-}
- */
-
-if ($stmt = $con->prepare('INSERT INTO chooses (account_id, choice_id) VALUES (?, ?)')) {
-  $stmt->bind_param('ss', $id, $choice_id);
-  $stmt->execute();
-  echo 'Entries successfully stored!';
-} else {
-  echo 'Could not prepare statement';
-}
-$stmt->close();
 
 $con->close();
 ?>
--- quiz.php
+++ quiz.php
@@ -9,12 +9,11 @@ if (!isset($_SESSION['loggedin'])) {
 // Include config
 require_once "config.php";
 
-// We don't have the password or email info stored in sessions so instead we can get the results from the database.
-$stmt = $con->prepare('SELECT * FROM questions WHERE question_id=2');
+$stmt = $con->prepare('SELECT * FROM questions');
 // In this case we can use the account ID to get the account info.
 //$stmt->bind_param('i', $_SESSION['id']);
 $stmt->execute();
-$stmt->bind_result($question_id, $question);
+$stmt->bind_result($question_id[][], $question[][]);
 $stmt->fetch();
 $stmt->close();
 ?>
@@ -30,19 +29,27 @@ $stmt->close();
   <body class="loggedin">
     <nav class="navtop">
       <div>
-	<h1>Like Like Love</h1>
-	<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-	<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+	      <h1>Like Like Love</h1>
+	      <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+	      <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
       </div>
     </nav>
     <div class="content">
       <h2>Quiz</h2>
       <div>
-      <p><?=$question?></p>
-	<form action="entry.php" method="post" autocomplete="off">
+	      <form action="entry.php" method="post" autocomplete="off">
+          <p><?=$question[0][1]?></p>
           <select name="q2" placeholder="Select" multiple >
-	    <option id="agree">Tend to Agree</option>
-	    <option id="disagree">Tend to Disagree</option>
+	          <option id="agree">Tend to Agree</option>
+	          <option id="disagree">Tend to Disagree</option>
+          <p><?=$question[1][1]?></p>
+          <select name="q3" placeholder="Select" multiple >
+	          <option id="agree">Tend to Agree</option>
+	          <option id="disagree">Tend to Disagree</option>
+          <p><?=$question[2][1]?></p>
+          <select name="q4" placeholder="Select" multiple >
+	          <option id="agree">Tend to Agree</option>
+	          <option id="disagree">Tend to Disagree</option>
           <input type="submit" value="submit" >
         </form>
       </div>
