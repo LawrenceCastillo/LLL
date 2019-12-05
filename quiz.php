676df02a2e1cb9e0f787ebbfc690ba1856e24b58
LawrenceCastillo <Lawrence@Lawrence-gabriel.com>
2019-12-05 10:56:49 -0500

    final quiz styling

--- quiz.php
+++ quiz.php
@@ -15,28 +15,30 @@ $result = mysqli_fetch_all($con->query($query), MYSQLI_ASSOC);
 
 <!DOCTYPE html>
 <html>
+
   <head>
-    <meta charset="utf-8">
-    <title>Quiz</title>
-    <link href="style.css" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>LikeLikeLove.com</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="css/screen.css">
   </head>
-  <body class="loggedin">
-    <nav class="navtop">
-      <div>
-	      <h1>Like Like Love</h1>
-	      <a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-	      <a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
-      </div>
-    </nav>
-    <div class="content">
-      <h2>Quiz</h2>
+
+  <body> 
+    <div id="page">
+      <header>
+        <a id="top"></a>
+        <a class="logo" title="LikeLikeLove.com" href="profile.php"><span>LikeLikeLove.com</span></a>
+        <div class="hero">
+        </div>
+      </header>
+
+      <h2 id="qpage">Personality Quiz To Find Your Match!</h2>
       <div>
 	<form action="entry.php" method="post" autocomplete="off"> 
 	  <?php for ( $i=0; $i < 8; $i++ ){ ?>
             <blockquote>
               <?php echo $result[$i]['question']; ?>
-              <select name="<?php echo $i+1 ?>"  >
+              <select type="text" name="<?php echo $i+1 ?>" id="question">
 	        <option id="agree">Tend to Agree</option>
 	        <option id="disagree">Tend to Disagree</option>
               </select>
@@ -45,6 +47,17 @@ $result = mysqli_fetch_all($con->query($query), MYSQLI_ASSOC);
           <input type="submit" value="submit" >
         </form>
       </div>
+
+      <nav>
+        <ul>
+          <li><a class="navigation" href="logout.php">LOGOUT</a></li>
+          <li><a class="navigation" href="quiz.php">TAKE THE QUIZ!</a></li>
+          <li><a class="navigation" href="profile.php">PROFILE</a></li>
+	  <li><a class="navigation" href="index.html">LOGIN</a></li>
+	  <li><a class="navigation" href="register.html">REGISTER</a></li>
+        </ul>
+      </nav>
+
     </div>
   </body>
 </html>
