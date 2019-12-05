d0d12de94993f68ce61263c5d1ab168503a267fb
LawrenceCastillo <lawrence.g.castillo@gmail.com>
2019-12-05 01:28:59 -0500

    cleanup

--- entry.php
+++ entry.php
@@ -26,36 +26,6 @@ foreach ($_POST as $key => $value) {
   $stmt->close();
 }
 
-/*
-if (!isset($_POST['q3'])){
-  if ( $_POST['q3'] == "Tend to Agree" ){ $choice_id = 3*2; $count++; }
-  else if ( $_POST['q3'] == "Tend to Disagree" ){ $choice_id = 3*2+1; $count++; }
-}
-
-if ($stmt = $con->prepare('INSERT INTO chooses (account_id, choice_id) VALUES (?, ?)')) {
-  $stmt->bind_param('ss', $id, $choice_id);
-  $stmt->execute();
-} else { die ( 'Something went wrong!' ); }
-$stmt->close();
-
-if (!isset($_POST['q4'])){
-  if ( $_POST['q4'] == "Tend to Agree" ) { $choice_id  = 4*2; $count++; }
-  else if ( $_POST['q4'] == "Tend to Disagree" ){ $choice_id = 4*2+1; $count++; }
-}
-
-if ($stmt = $con->prepare('INSERT INTO chooses (account_id, choice_id) VALUES (?, ?)')) {
-  $stmt->bind_param('ss', $id, $choice_id);
-  $stmt->execute();
-} else { die ( 'Something went wrong!' ); }
-$stmt->close();
- */
-
-// Make sure the submitted registration values are not empty.
-//if (empty($_POST['phonenumber']) || empty($_POST['zipcode']) || empty($_POST['lookingfor'])) {
-  // One or more values are empty.
-//  die ('Please complete the registration form');
-//}
-
 $con->close();
 ?>
 
